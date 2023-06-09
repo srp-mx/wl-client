3c936d998bfd5d7e08990e7d743918b2942ddf20
Santiago Romero <srp.devel@gmail.com>
2023-06-09 13:42:44 -0600

    Conexion y desconexion del servidor

--- wl-client.c
+++ wl-client.c
@@ -6,5 +6,12 @@
 int8_t
 main()
 {
+    // Looks for an active Wayland server. Returns the connection to that server.
+    struct wl_display* disp = wl_display_connect(0);
+    if (!disp)
+        return -1;
+
+    // It's good practice to disconnect your display when you're done with it.
+    wl_display_disconnect(disp);
     return 0;
 }
